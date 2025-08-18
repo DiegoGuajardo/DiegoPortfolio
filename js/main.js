aad7ba27aa566794574eaa5272b19d49d37bbd12
DiegoGGuajardo4@gmail.com <DiegoGGuajardo4@gmail.com>
2025-08-18 11:49:37 -0400

    Added features

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
 console.log("Portfolio site loaded!");
+
+window.addEventListener('load', () => {
+  const welcome = document.getElementById('welcome-screen');
+
+  // Check if welcome screen has already been shown
+  if (!sessionStorage.getItem('welcomeShown')) {
+    // Show it
+    setTimeout(() => {
+      welcome.classList.add('hide');
+
+      setTimeout(() => {
+        welcome.classList.add('hidden');
+      }, 1000);
+    }, 2500);
+
+    // Mark as shown
+    sessionStorage.setItem('welcomeShown', 'true');
+  } else {
+    // Skip animation if already shown
+    welcome.classList.add('hidden');
+  }
+});
